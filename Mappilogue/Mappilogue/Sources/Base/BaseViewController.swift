f453feaa58242dff3f82018fead82550078c5adb
hyemib <hyem0127@gmail.com>
2023-05-17 21:22:23 +0900

    Initial setting

---
+++ Mappilogue/Mappilogue/Sources/Base/BaseViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BaseViewController.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/05/17.
+//
+
+import UIKit
+
+protocol BaseViewControllerProtocol {
+    func setupProperty()
+    func setupHierarchy()
+    func setupDelegate()
+    func setupLayout()
+}
+
+class BaseViewController: UIViewController, BaseViewControllerProtocol {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        setupProperty()
+        setupHierarchy()
+        setupDelegate()
+        setupLayout()
+    }
+    
+    func setupProperty() {}
+    
+    func setupHierarchy() {}
+    
+    func setupDelegate() {}
+    
+    func setupLayout() {}
+}
+
---
+++ Mappilogue/Mappilogue/Sources/Presenter/Gathering/GatheringViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GatheringViewController.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/05/17.
+//
+
+import UIKit
+
+class GatheringViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
