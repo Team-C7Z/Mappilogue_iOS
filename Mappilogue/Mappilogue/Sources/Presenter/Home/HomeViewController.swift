c1277f4548f697315c372d162acf9abe1766d091
hyemib <hyem0127@gmail.com>
2023-06-27 07:17:50 +0900

    #3 - feat: 홈 - 네비게이션 바

--- Mappilogue/Mappilogue/Sources/Presenter/Home/HomeViewController.swift
+++ Mappilogue/Mappilogue/Sources/Presenter/Home/HomeViewController.swift
@@ -7,23 +7,19 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: NavigationBarViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setNavigationBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setNavigationBar() {
+        title = ""
+        
+        let logoImage = UIImage(named: "home_logo")?.withRenderingMode(.alwaysOriginal)
+        let buttonItem = UIBarButtonItem(image: logoImage, style: .plain, target: self, action: nil)
+        navigationItem.leftBarButtonItem = buttonItem
     }
-    */
-
 }
