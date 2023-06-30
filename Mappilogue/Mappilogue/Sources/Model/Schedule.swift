34ab69924d399ac64776e42bd91ec68bc32edbac
hyemib <hyem0127@gmail.com>
2023-06-30 09:18:15 +0900

    add: 색상 추가

--- Mappilogue/Mappilogue/Sources/Model/Schedule.swift
+++ Mappilogue/Mappilogue/Sources/Model/Schedule.swift
@@ -21,12 +21,12 @@ func generateDummyScheduleData(scheduleCount: Int) -> [Schedule] {
     if scheduleCount == 0 {
         
     } else if scheduleCount == 1 {
-        let schedule = Schedule(title: "아빠랑 데이트 🏃🏻", location: ["춘배식당", "카페 문", "아주 건강해지는 제이슨 건강어쩌구어쩌구"], time: ["9:00 AM", "10:00 AM", "1:00 PM"], color: .yellow)
+        let schedule = Schedule(title: "아빠랑 데이트 🏃🏻", location: ["춘배식당", "카페 문", "아주 건강해지는 제이슨 건강어쩌구어쩌구"], time: ["9:00 AM", "10:00 AM", "1:00 PM"], color: .colorB1E9BE)
         schedules = [schedule]
     } else {
-        let schedule1 = Schedule(title: "아빠랑 데이트 🏃🏻", location: ["춘배식당", "카페 문", "아주 건강해지는 제이슨 건강어쩌구어쩌구"], time: ["9:00 AM", "10:00 AM", "1:00 PM"], color: .yellow)
-        let schedule2 = Schedule(title: "러닝메이트 회의", location: ["공간대여 산", "에나 파스타"], time: ["3:00 PM", "6:00 PM"], color: .green)
-        let schedule3 = Schedule(title: "CS 스터디", location: ["운영체제 회의실", "자료구조 카페"], time: ["1:00 PM", "3:00 PM"], color: .gray)
+        let schedule1 = Schedule(title: "아빠랑 데이트 🏃🏻", location: ["춘배식당", "카페 문", "아주 건강해지는 제이슨 건강어쩌구어쩌구"], time: ["9:00 AM", "10:00 AM", "1:00 PM"], color: .colorB1E9BE)
+        let schedule2 = Schedule(title: "러닝메이트 회의", location: ["공간대여 산", "에나 파스타"], time: ["3:00 PM", "6:00 PM"], color: .colorBAD7FA)
+        let schedule3 = Schedule(title: "CS 스터디", location: ["운영체제 회의실", "자료구조 카페"], time: ["1:00 PM", "3:00 PM"], color: .colorE6C3F2)
         
         schedules = [schedule1, schedule2, schedule3]
     }
