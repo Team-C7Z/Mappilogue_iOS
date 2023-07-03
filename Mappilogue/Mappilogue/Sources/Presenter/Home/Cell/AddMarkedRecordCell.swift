aa27a8abca81eefb37d4eba506b1962c8ca80a46
hyemib <hyem0127@gmail.com>
2023-07-03 11:15:33 +0900

    feat: 기록 추가 UI 구현

---
+++ Mappilogue/Mappilogue/Sources/Presenter/Home/Cell/AddMarkedRecordCell.swift
@@ -0,0 +1,38 @@
+//
+//  AddMarkedRecordCell.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/07/03.
+//
+
+import UIKit
+
+class AddMarkedRecordCell: BaseCollectionViewCell {
+    static let registerId = "\(AddMarkedRecordCell.self)"
+    
+    private let addMarkedRecordButton = UIButton()
+
+    override func setupProperty() {
+        super.setupProperty()
+        
+        contentView.backgroundColor = .colorF5F3F0
+        contentView.layer.cornerRadius = 12
+        
+        addMarkedRecordButton.setImage(UIImage(named: "addMarkedRecord"), for: .normal)
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        contentView.addSubview(addMarkedRecordButton)
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+      
+        addMarkedRecordButton.snp.makeConstraints {
+            $0.centerX.centerY.equalTo(contentView)
+            $0.width.height.equalTo(44)
+        }
+    }
+}
