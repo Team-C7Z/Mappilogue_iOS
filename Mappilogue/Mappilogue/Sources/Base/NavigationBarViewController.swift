707435161a4ed71a206f0f22693d24d5bd46a4eb
hyemib <hyem0127@gmail.com>
2023-07-08 08:26:03 +0900

    feat: NavigationBarViewController 추가

---
+++ Mappilogue/Mappilogue/Sources/Base/NavigationBarViewController.swift
@@ -0,0 +1,28 @@
+//
+//  NavigationBarViewController.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/07/08.
+//
+
+import UIKit
+
+class NavigationBarViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpRightBarButtonItem()
+    }
+    
+    func setUpRightBarButtonItem() {
+        let notificationImage = UIImage(named: "notification")
+        let notificationButtonItem = UIBarButtonItem(image: notificationImage, style: .plain, target: self, action: #selector(rightBarButtonItemTapped))
+        notificationButtonItem.tintColor = .color1C1C1C
+        navigationItem.rightBarButtonItem = notificationButtonItem
+    }
+    
+    @objc func rightBarButtonItemTapped() {
+        print("notification")
+    }
+}
