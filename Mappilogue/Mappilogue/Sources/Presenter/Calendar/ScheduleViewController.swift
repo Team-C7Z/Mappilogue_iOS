6b93a9604068f10affea2e314c867b77fecad957
hyemib <hyem0127@gmail.com>
2023-07-13 15:03:25 +0900

    feat: cell 선택 시 뷰 이동

---
+++ Mappilogue/Mappilogue/Sources/Presenter/Calendar/ScheduleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleViewController.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/07/13.
+//
+
+import UIKit
+
+class ScheduleViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+
+}
