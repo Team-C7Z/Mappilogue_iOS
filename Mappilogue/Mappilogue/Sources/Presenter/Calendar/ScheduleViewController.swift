2b1566799e967b65f60b1195078e271de8fcd4d0
hyemib <hyem0127@gmail.com>
2023-07-13 16:18:27 +0900

    feat: 일정 상세 보기 UI 구성

--- Mappilogue/Mappilogue/Sources/Presenter/Calendar/ScheduleViewController.swift
+++ Mappilogue/Mappilogue/Sources/Presenter/Calendar/ScheduleViewController.swift
@@ -9,10 +9,109 @@ import UIKit
 
 class ScheduleViewController: BaseViewController {
 
+    private let scheduleView = UIView()
+    private let dateLabel = UILabel()
+    private let lunarDateLabel = UILabel()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(ScheduleInfoCell.self, forCellWithReuseIdentifier: ScheduleInfoCell.registerId)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        view.backgroundColor = .color000000.withAlphaComponent(0.4)
+        
+        scheduleView.layer.cornerRadius = 24
+        scheduleView.backgroundColor = .colorF9F8F7
+        
+        dateLabel.text = "5월 16일"
+        dateLabel.font = .title01
+        dateLabel.textColor = .color1C1C1C
+        
+        lunarDateLabel.text = "음력 3월 27일"
+        lunarDateLabel.font = .body02
+        lunarDateLabel.textColor = .color707070
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        view.addSubview(scheduleView)
+        scheduleView.addSubview(dateLabel)
+        scheduleView.addSubview(lunarDateLabel)
+        scheduleView.addSubview(collectionView)
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        scheduleView.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.height.equalTo(429)
+        }
+        
+        dateLabel.snp.makeConstraints {
+            $0.top.equalTo(scheduleView).offset(30)
+            $0.leading.equalTo(scheduleView).offset(20)
+        }
+        
+        lunarDateLabel.snp.makeConstraints {
+            $0.centerY.equalTo(dateLabel)
+            $0.leading.equalTo(dateLabel.snp.trailing).offset(6)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(scheduleView).offset(78)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
 
-  
+extension ScheduleViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ScheduleInfoCell.registerId, for: indexPath) as? ScheduleInfoCell else { return UICollectionViewCell() }
+        
+        //cell.configure(year: selectedDate.year, month: selectedDate.month, weekIndex: indexPath.row)
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
     }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width-40, height: 52)
+    }
+    
+    // 수평 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    // 수직 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
