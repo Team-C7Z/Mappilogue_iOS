5ed9baf4e6b78d93774a34f8b935c77bbe709861
hyemib <hyem0127@gmail.com>
2023-07-13 16:53:00 +0900

    refactor: 코드 리팩토링

--- Mappilogue/Mappilogue/Sources/Presenter/Resuable/AddScheduleButton.swift
+++ Mappilogue/Mappilogue/Sources/Presenter/Resuable/AddScheduleButton.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class AddScheduleButton: UIButton {
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-     }
-    
     init() {
         super.init(frame: .zero)
         
         setImage(UIImage(named: "addSchedule"), for: .normal)
         frame.size = .init(width: 55, height: 55)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
 }
