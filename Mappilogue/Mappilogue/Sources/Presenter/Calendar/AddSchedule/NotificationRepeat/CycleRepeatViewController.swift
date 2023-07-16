d7deea0c63f9edd1ca123c148d9e77c2d2ff51f1
hyemib <hyem0127@gmail.com>
2023-07-16 15:09:13 +0900

    feat: 주기 별 버튼 UI 구성

--- Mappilogue/Mappilogue/Sources/Presenter/Calendar/AddSchedule/NotificationRepeat/CycleRepeatViewController.swift
+++ Mappilogue/Mappilogue/Sources/Presenter/Calendar/AddSchedule/NotificationRepeat/CycleRepeatViewController.swift
@@ -7,11 +7,91 @@
 
 import UIKit
 
-class CycleRepeatViewController: UIViewController {
+class CycleRepeatViewController: BaseViewController {
+    var selectedCycle: String?
+    
+    private let stackView = UIStackView()
+    private var dailyButton = UIButton()
+    private let monthlyButton = UIButton()
+    private let yearlyButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .yellow
+    }
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        
+        dailyButton.setTitle("매일", for: .normal)
+        dailyButton.setTitleColor(.colorC9C6C2, for: .normal)
+        dailyButton.titleLabel?.font = .title02
+        dailyButton.layer.cornerRadius = 16
+        dailyButton.layer.borderWidth = 2
+        dailyButton.layer.borderColor = UIColor.colorC9C6C2.cgColor
+        dailyButton.addTarget(self, action: #selector(cycleButtonTapped), for: .touchUpInside)
+        
+        monthlyButton.setTitle("매월", for: .normal)
+        monthlyButton.setTitleColor(.colorC9C6C2, for: .normal)
+        monthlyButton.titleLabel?.font = .title02
+        monthlyButton.layer.cornerRadius = 16
+        monthlyButton.layer.borderWidth = 2
+        monthlyButton.layer.borderColor = UIColor.colorC9C6C2.cgColor
+        monthlyButton.addTarget(self, action: #selector(cycleButtonTapped), for: .touchUpInside)
+        
+        yearlyButton.setTitle("매년", for: .normal)
+        yearlyButton.setTitleColor(.colorC9C6C2, for: .normal)
+        yearlyButton.titleLabel?.font = .title02
+        yearlyButton.layer.cornerRadius = 16
+        yearlyButton.layer.borderWidth = 2
+        yearlyButton.layer.borderColor = UIColor.colorC9C6C2.cgColor
+        yearlyButton.addTarget(self, action: #selector(cycleButtonTapped), for: .touchUpInside)
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        view.addSubview(stackView)
+        [dailyButton, monthlyButton, yearlyButton].forEach {
+            stackView.addArrangedSubview($0)
+        }
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(view).offset(18)
+            $0.leading.equalTo(view)
+        }
+        
+        dailyButton.snp.makeConstraints {
+            $0.width.equalTo(64)
+            $0.height.equalTo(32)
+        }
+
+        monthlyButton.snp.makeConstraints {
+            $0.width.equalTo(64)
+            $0.height.equalTo(32)
+        }
+
+        yearlyButton.snp.makeConstraints {
+            $0.width.equalTo(64)
+            $0.height.equalTo(32)
+        }
+    }
+
+    @objc func cycleButtonTapped(_ sender: UIButton) {
+        if let title = sender.titleLabel?.text, title == "매일" {
+            selectedCycle = title
+        } else if let title = sender.titleLabel?.text, title == "매월" {
+            selectedCycle = title
+        } else if let title = sender.titleLabel?.text, title == "매년" {
+            selectedCycle = title
+        }
     }
 }
