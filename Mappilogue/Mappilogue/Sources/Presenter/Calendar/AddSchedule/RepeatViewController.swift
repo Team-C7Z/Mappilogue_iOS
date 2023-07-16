ebad1f8550ec7052eb97d6e4901b85e4807261a4
hyemib <hyem0127@gmail.com>
2023-07-16 11:22:54 +0900

    feat: 뷰 이동, 네비게이션 바 구성

---
+++ Mappilogue/Mappilogue/Sources/Presenter/Calendar/AddSchedule/RepeatViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RepeatViewController.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/07/16.
+//
+
+import UIKit
+
+class RepeatViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        setNavigationBar()
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+    }
+    
+    private func setNavigationBar() {
+        title = "알림"
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "back2"), for: .normal)
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        let barButtonItem = UIBarButtonItem(customView: button)
+        navigationItem.leftBarButtonItem = barButtonItem
+    }
+    
+    @objc func backButtonTapped(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+}
