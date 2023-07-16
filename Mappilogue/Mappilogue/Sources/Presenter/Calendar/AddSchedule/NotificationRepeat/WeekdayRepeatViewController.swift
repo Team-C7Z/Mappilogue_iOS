040d7ec99a4bbb0723c1a465f0ccb6c6732da0cf
hyemib <hyem0127@gmail.com>
2023-07-16 13:18:08 +0900

    feat: 반복 카테고리 구현

---
+++ Mappilogue/Mappilogue/Sources/Presenter/Calendar/AddSchedule/NotificationRepeat/WeekdayRepeatViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeekdayRepeatViewController.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/07/16.
+//
+
+import UIKit
+
+class WeekdayRepeatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+}
