2a526310fd36c505ac2257c960fc9dc45d1fe65a
hyemib <hyem0127@gmail.com>
2023-08-01 12:48:52 +0900

    refactor: 코드 리팩토링

--- Mappilogue/Mappilogue/Sources/Presenter/Record/MarkerView.swift
+++ Mappilogue/Mappilogue/Sources/Presenter/Record/MarkerView.swift
@@ -1,5 +1,5 @@
 //
-//  markerView.swift
+//  MarkerView.swift
 //  Mappilogue
 //
 //  Created by hyemi on 2023/08/01.
@@ -15,18 +15,23 @@ class MarkerView: BaseView {
         super.init(frame: frame)
         
         markerImage = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
-        markerImage.image = UIImage(named: "marker")
+        addSubview(markerImage)
         
         recordImage = UIImageView(frame: CGRect(x: (frame.width - 32) / 2, y: 8, width: 32, height: 32))
-        recordImage.image = UIImage(named: "marker_logo")
-        recordImage.layer.cornerRadius = 16
-       
         markerImage.addSubview(recordImage)
-        addSubview(markerImage)
+        
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        markerImage.image = UIImage(named: "marker")
+        
+        recordImage.image = UIImage(named: "marker_logo")
+        recordImage.layer.cornerRadius = 16
     }
     
     func configure(image: String, color: UIColor) {
