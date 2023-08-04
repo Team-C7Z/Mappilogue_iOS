c677e47d7f0683530f1c2e42b3c834c760389685
hyemib <hyem0127@gmail.com>
2023-08-04 16:47:20 +0900

    feat: 지도에서 표시할 카테고리 선택 구현

--- Mappilogue/Mappilogue/Sources/Presenter/Record/MyRecord/Cell/CategorySelectionCell.swift
+++ Mappilogue/Mappilogue/Sources/Presenter/Record/MyRecord/Cell/CategorySelectionCell.swift
@@ -10,6 +10,8 @@ import UIKit
 class CategorySelectionCell: BaseCollectionViewCell {
     static let registerId = "\(CategorySelectionCell.self)"
 
+    private var isSelection: Bool = false
+    
     private let categoryLabel = UILabel()
     private let categoryImage = UIImageView()
     
@@ -18,12 +20,12 @@ class CategorySelectionCell: BaseCollectionViewCell {
         
         contentView.layer.cornerRadius = 16
         contentView.layer.applyShadow()
-        contentView.backgroundColor = .colorF9F8F7
+        contentView.backgroundColor = .colorEAE6E1
         
         categoryLabel.textColor = .color1C1C1C
         categoryLabel.font = .caption02
         
-        categoryImage.image = UIImage(named: "deleteMapCategory")
+        categoryImage.image = UIImage(named: "addCategory")
     }
     
     override func setupHierarchy() {
@@ -44,13 +46,23 @@ class CategorySelectionCell: BaseCollectionViewCell {
         categoryImage.snp.makeConstraints {
             $0.leading.equalTo(categoryLabel.snp.trailing).offset(8)
             $0.centerY.equalTo(contentView)
-            $0.width.height.equalTo(8)
+            $0.width.height.equalTo(10)
         }
     }
     
-    func configure(_ title: String, isSelection: Bool) {
+    func configure(_ title: String) {
         categoryLabel.text = title
+    }
+    
+    func selectCateogry() {
+        isSelection = !isSelection
         categoryImage.image = UIImage(named: isSelection ? "deleteCategory" : "addCategory")
         contentView.backgroundColor = isSelection ? .colorF9F8F7 : .colorEAE6E1
+        
+        categoryImage.snp.updateConstraints {
+            $0.width.height.equalTo(isSelection ? 8 : 10)
+        }
+        
+        self.layoutIfNeeded()
     }
 }
