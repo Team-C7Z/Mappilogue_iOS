3e7f0b05a326f39f4fc1ca988371c92aa6edc9bd
hyemib <hyem0127@gmail.com>
2023-08-06 12:03:20 +0900

    feat: 메인 UI 구성

---
+++ Mappilogue/Mappilogue/Sources/Presenter/My/Cell/ProfileCell.swift
@@ -0,0 +1,74 @@
+//
+//  ProfileCell.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/08/06.
+//
+
+import UIKit
+
+class ProfileCell: BaseCollectionViewCell {
+    static let registerId = "\(ProfileCell.self)"
+    
+    private let profileImage = UIImageView()
+    private let profileName = UILabel()
+    private let profileEmail = UILabel()
+    private let moveImage = UIImageView()
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        profileImage.layer.cornerRadius = 56 / 2
+        profileImage.backgroundColor = .colorF5F3F0
+        profileImage.image = UIImage(named: "profile")
+        
+        profileName.text = "맵필로그 님"
+        profileName.textColor = .color000000
+        profileName.font = .title02
+        
+        profileEmail.text = "mappilogue@kakao.com"
+        profileEmail.textColor = .color707070
+        profileEmail.font = .caption01
+        
+        moveImage.image = UIImage(named: "my_move")
+        moveImage.tintColor = .color707070
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        contentView.addSubview(profileImage)
+        contentView.addSubview(profileName)
+        contentView.addSubview(profileEmail)
+        contentView.addSubview(moveImage)
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        profileImage.snp.makeConstraints {
+            $0.leading.centerY.equalTo(contentView)
+            $0.width.height.equalTo(56)
+        }
+        
+        profileName.snp.makeConstraints {
+            $0.leading.equalTo(profileImage.snp.trailing).offset(12)
+            $0.top.equalTo(contentView).offset(14)
+        }
+        
+        profileEmail.snp.makeConstraints {
+            $0.leading.equalTo(profileName)
+            $0.top.equalTo(contentView).offset(38)
+        }
+        
+        moveImage.snp.makeConstraints {
+            $0.centerY.equalTo(contentView)
+            $0.trailing.equalTo(contentView).offset(-1)
+            $0.width.equalTo(7)
+            $0.height.equalTo(14)
+        }
+    }
+    
+    func configure() {
+    }
+}
---
+++ Mappilogue/Mappilogue/Sources/Presenter/My/Cell/VersionCell.swift
@@ -0,0 +1,78 @@
+//
+//  VersionCell.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/08/06.
+//
+
+import UIKit
+
+class VersionCell: BaseCollectionViewCell {
+    static let registerId = "\(VersionCell.self)"
+    
+    private let versionInfoLabel = UILabel()
+    private let versionLabel = UILabel()
+    private let updateLabel = UILabel()
+    private let moveImage = UIImageView()
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        contentView.layer.cornerRadius = 12
+        contentView.backgroundColor = .colorF5F3F0
+        
+        versionInfoLabel.text = "버전정보"
+        versionInfoLabel.textColor = .color000000
+        versionInfoLabel.font = .body02
+        
+        versionLabel.text = "0.0.1 / 0.0.1"
+        versionLabel.textColor = .color707070
+        versionLabel.font = .body02
+        
+        updateLabel.text = "업데이트"
+        updateLabel.textColor = .colorC9C6C2
+        updateLabel.font = .body02
+        
+        moveImage.image = UIImage(named: "my_move")
+        moveImage.tintColor = .colorC9C6C2
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        contentView.addSubview(versionInfoLabel)
+        contentView.addSubview(versionLabel)
+        contentView.addSubview(updateLabel)
+        contentView.addSubview(moveImage)
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        versionInfoLabel.snp.makeConstraints {
+            $0.centerY.equalTo(contentView)
+            $0.leading.equalTo(contentView).offset(12)
+        }
+        
+        versionLabel.snp.makeConstraints {
+            $0.centerY.equalTo(contentView)
+            $0.leading.equalTo(versionInfoLabel.snp.trailing).offset(8)
+        }
+        
+        updateLabel.snp.makeConstraints {
+            $0.centerY.equalTo(contentView)
+            $0.trailing.equalTo(moveImage.snp.leading).offset(-8)
+        }
+        
+        moveImage.snp.makeConstraints {
+            $0.centerY.equalTo(contentView)
+            $0.trailing.equalTo(contentView).offset(-12)
+            $0.width.equalTo(7)
+            $0.height.equalTo(14)
+        }
+        
+    }
+    
+    func configure() {
+    }
+}
