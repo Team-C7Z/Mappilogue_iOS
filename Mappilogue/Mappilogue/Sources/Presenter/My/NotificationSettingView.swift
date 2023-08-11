ca02b7e9508e51d22a17f64c03267655d2324052
hyemi <hyem0127@gmail.com>
2023-08-11 14:06:19 +0900

    feat: 알림 스위치 구현

---
+++ Mappilogue/Mappilogue/Sources/Presenter/My/NotificationSettingView.swift
@@ -0,0 +1,57 @@
+//
+//  NotificationSettingView.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/08/06.
+//
+
+import UIKit
+
+class NotificationSettingView: BaseView {
+    var onSwitchTapped: (() -> Void)?
+    
+    private let notificationLabel = UILabel()
+    private let notificationSwitch = NotificationSwitchButton()
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        backgroundColor = .colorF9F8F7
+        notificationLabel.textColor = .color1C1C1C
+        notificationLabel.font = .body02
+        
+        notificationSwitch.onSwitchTapped = {
+            self.onSwitchTapped?()
+        }
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        addSubview(notificationLabel)
+        addSubview(notificationSwitch)
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        self.snp.makeConstraints {
+            $0.height.equalTo(48)
+        }
+        
+        notificationLabel.snp.makeConstraints {
+            $0.top.equalTo(self).offset(14)
+            $0.leading.equalTo(self)
+        }
+        
+        notificationSwitch.snp.makeConstraints {
+            $0.top.equalTo(self).offset(8)
+            $0.trailing.equalTo(self)
+        }
+    }
+    
+    func configure(title: String, isSwitch: Bool) {
+        notificationLabel.text = title
+        notificationSwitch.configure(isSwitch)
+    }
+}
