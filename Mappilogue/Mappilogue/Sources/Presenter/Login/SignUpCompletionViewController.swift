6a1afe6c25caa0943e9d38b8ca1aa426bb546d67
hyemi <hyem0127@gmail.com>
2023-08-30 20:04:15 +0900

    feat: 회원가입 완료 뷰 UI 구성

--- Mappilogue/Mappilogue/Sources/Presenter/Login/SignUpCompletionViewController.swift
+++ Mappilogue/Mappilogue/Sources/Presenter/Login/SignUpCompletionViewController.swift
@@ -7,23 +7,54 @@
 
 import UIKit
 
-class SignUpCompletionViewController: UIViewController {
-
+class SignUpCompletionViewController: BaseViewController {
+    private let completionRoundView = UIView()
+    private let completionImage = UIImageView()
+    private let completionLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setupProperty() {
+        super.setupProperty()
+    
+        completionRoundView.layer.cornerRadius = 35 / 2
+        completionRoundView.layer.borderWidth = 2
+        completionRoundView.layer.borderColor = UIColor.color707070.cgColor
+        completionRoundView.backgroundColor = .clear
+    
+        completionImage.image = UIImage(named: "login_completion")
+        
+        completionLabel.text = "회원가입이 완료되었어요"
+        completionLabel.textColor = .color1C1C1C
+        completionLabel.font = .title01
     }
-    */
 
+    override func setupHierarchy() {
+        super.setupHierarchy()
+
+        view.addSubview(completionRoundView)
+        completionRoundView.addSubview(completionImage)
+        view.addSubview(completionLabel)
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        completionRoundView.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-362)
+            $0.centerX.equalToSuperview()
+        }
+        
+        completionImage.snp.makeConstraints {
+            $0.center.equalTo(completionRoundView)
+        }
+        
+        completionLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-307)
+            $0.centerX.equalToSuperview()
+        }
+    }
 }
