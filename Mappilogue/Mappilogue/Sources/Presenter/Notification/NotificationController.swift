0bf976ca69faca7f2ac8f7bf3c1b1756efc0cd19
hyemi <hyem0127@gmail.com>
2023-09-13 20:04:12 +0900

    feat: 텅뷰 구현

--- Mappilogue/Mappilogue/Sources/Presenter/Notification/NotificationController.swift
+++ Mappilogue/Mappilogue/Sources/Presenter/Notification/NotificationController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class NotificationController: BaseViewController {
+    let notificationData: [NotificationData] = []
+    let announcementData: [AnnouncementData] = []
+    
+    var notificationType: NotificationType = .notification
+    
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.separatorStyle = .none
@@ -15,7 +20,7 @@ class NotificationController: BaseViewController {
         tableView.sectionHeaderHeight = 0
         tableView.sectionFooterHeight = 0
         tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
-        tableView.register(TodayScheduleCell.self, forCellReuseIdentifier: TodayScheduleCell.registerId)
+        tableView.register(EmptyNotificationCell.self, forCellReuseIdentifier: EmptyNotificationCell.registerId)
         tableView.register(NotificationAnnouncementHeaderView.self, forHeaderFooterViewReuseIdentifier: NotificationAnnouncementHeaderView.registerId)
         tableView.delegate = self
         tableView.dataSource = self
@@ -45,23 +50,45 @@ class NotificationController: BaseViewController {
 
 extension NotificationController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        switch notificationType {
+        case .notification:
+            return notificationData.isEmpty ? 1 : notificationData.count
+        case .announcement:
+            return announcementData.isEmpty ? 1 : announcementData.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeEmptyScheduleCell.registerId, for: indexPath) as? HomeEmptyScheduleCell else { return UITableViewCell() }
-        cell.selectionStyle = .none
-        
-        return cell
+        switch notificationType {
+        case .notification:
+            // if notificationData.isEmpty {
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: EmptyNotificationCell.registerId, for: indexPath) as? EmptyNotificationCell else { return UITableViewCell() }
+                cell.configure(notificationType: .notification)
+                cell.selectionStyle = .none
+            //}
+            return cell
+        case .announcement:
+           // if announcementData.isEmpty {
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: EmptyNotificationCell.registerId, for: indexPath) as? EmptyNotificationCell else { return UITableViewCell() }
+                cell.configure(notificationType: .announcement)
+                cell.selectionStyle = .none
+           // }
+            return cell
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 40
+        switch notificationType {
+        case .notification:
+            return notificationData.isEmpty ? tableView.frame.height - 100 : 50
+        case .announcement:
+            return announcementData.isEmpty ? tableView.frame.height - 100 : 40
+        }
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: NotificationAnnouncementHeaderView.registerId) as? NotificationAnnouncementHeaderView else { return UIView() }
-        
+        headerView.delegate = self
         return headerView
     }
     
@@ -81,5 +108,13 @@ extension NotificationController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
       
+    }
+}
+
+extension NotificationController: NotificationTypeDelegate {
+    func categoryButtonTapped(_ notificationType: NotificationType) {
+        self.notificationType = notificationType
+
+        tableView.reloadData()
     }
 }
--- Mappilogue/Mappilogue/Sources/Presenter/Notification/NotificationType.swift
+++ Mappilogue/Mappilogue/Sources/Presenter/Notification/NotificationType.swift
@@ -8,6 +8,6 @@
 import UIKit
 
 enum NotificationType {
-    case noti
+    case notification
     case announcement
 }
