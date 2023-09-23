d1bd4eb8025fa89847fdaa424f54216cee272a5a
hyemi <hyem0127@gmail.com>
2023-09-23 11:57:13 +0900

    feat: 선택한 시작 및 종료 날짜에 배경 색상 표시

---
+++ Mappilogue/Mappilogue/Sources/Presenter/Calendar/AddSchedule/AddScheduleDateType.swift
@@ -0,0 +1,14 @@
+//
+//  AddScheduleDateType.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/09/23.
+//
+
+import Foundation
+
+enum AddScheduleDateType {
+    case startDate
+    case endDate
+    case unKnown
+}
