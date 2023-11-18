34273abcb3609f27c3b6fcd046625e212915185b
hyemi <hyem0127@gmail.com>
2023-11-18 15:31:51 +0900

    rename: 파일 이동

---
+++ Mappilogue/Mappilogue/Sources/Data/APIManager/ColorManager.swift
@@ -0,0 +1,29 @@
+//
+//  ColorManager.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/11/18.
+//
+
+import Foundation
+import Combine
+
+class ColorManager: ColorAPI {
+    private let baseURL = URL(string: "\(Environment.baseURL)/api/v1/schedules/colors")!
+    
+    func getCategory(title: String) -> AnyPublisher<BaseDTO<[ColorListDTO]>, Error> {
+        var request = URLRequest(url: baseURL)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { data, response in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: BaseDTO<[ColorListDTO]>.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
