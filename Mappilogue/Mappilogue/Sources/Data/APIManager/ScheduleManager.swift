22c70700a3e34ce20a2093c5f0274e8e915d1f03
hyemi <hyem0127@gmail.com>
2023-11-25 00:48:09 +0900

    feat: 일정 수정 API 연동

--- Mappilogue/Mappilogue/Sources/Data/APIManager/ScheduleManager.swift
+++ Mappilogue/Mappilogue/Sources/Data/APIManager/ScheduleManager.swift
@@ -90,6 +90,68 @@ class ScheduleManager: ScheduleAPI {
             .eraseToAnyPublisher()
     }
     
+    func updateSchedule(id: Int, schedule: Schedule) -> AnyPublisher<Void, Error> {
+        let url = URL(string: "\(baseURL)/\(id)")!
+        var request = setupRequest(for: url, method: "PUT")
+        
+        var requestParameters: [String: Any] = [
+            "colorId": schedule.colorId,
+            "startDate": schedule.startDate,
+            "endDate": schedule.endDate
+        ]
+        
+        if let title = schedule.title {
+            requestParameters["title"] = title
+        }
+        
+        if let alarmOptions = schedule.alarmOptions {
+            requestParameters["alarmOptions"] = alarmOptions
+        }
+        
+        if let area = schedule.area {
+            requestParameters["area"] = area.map { areaList in
+                return [
+                    "date": areaList.date,
+                    "value": areaList.value.map { location in
+                        var locationInfo: [String: Any] = [
+                            "name": location.name
+                        ]
+                        if let streetAddress = location.streetAddress {
+                            locationInfo["streetAddress"] = streetAddress
+                        }
+                        if let latitude = location.latitude {
+                            locationInfo["latitude"] = latitude
+                        }
+                        if let longitude = location.longitude {
+                            locationInfo["longitude"] = longitude
+                        }
+                        if let time = location.time {
+                            locationInfo["time"] = time
+                        }
+
+                        return locationInfo
+                    }
+                ] as [String: Any]
+            }
+        }
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: requestParameters)
+        } catch {
+            return Fail(error: CategoryAPIError.serializationError(error)).eraseToAnyPublisher()
+        }
+
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { _, response in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 204 else {
+                    throw URLError(.badServerResponse)
+                }
+             
+                return
+            }
+            .eraseToAnyPublisher()
+    }
+    
     private func setupRequest(for url: URL, method: String) -> URLRequest {
         var request = URLRequest(url: url)
         request.httpMethod = method
