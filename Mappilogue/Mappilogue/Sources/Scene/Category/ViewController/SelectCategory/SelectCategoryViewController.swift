5add09fefd941c6c6bf02028133486285e2b3442
hyemi <hyem0127@gmail.com>
2023-11-26 21:36:53 +0900

    refactor: 카테고리 조회 viewmodel 코드 수정

--- Mappilogue/Mappilogue/Sources/Scene/Category/ViewController/SelectCategory/SelectCategoryViewController.swift
+++ Mappilogue/Mappilogue/Sources/Scene/Category/ViewController/SelectCategory/SelectCategoryViewController.swift
@@ -122,19 +122,15 @@ extension SelectCategoryViewController: UICollectionViewDelegate, UICollectionVi
 extension SelectCategoryViewController {
     private func loadCategoryData() {
         categoryViewModel.getCategory()
-            .sink { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print("error: \(error.localizedDescription)")
-                }
-            } receiveValue: { response in
-                guard let result = response.result else { return }
+     
+        categoryViewModel.$categoryResult
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: { result in
+                guard let result else { return }
                 
                 self.categories = result.markCategories
                 self.collectionView.reloadData()
-            }
+            })
             .store(in: &categoryViewModel.cancellables)
     }
 }
