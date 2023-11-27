ad3945aa530aef1393bc72d2f42653ee90f40679
hyemi <hyem0127@gmail.com>
2023-11-27 11:33:22 +0900

    design: 로그인 뷰 UI 변경

--- Mappilogue/Mappilogue/Sources/Scene/Login/ViewController/SignUpCompletionViewController.swift
+++ Mappilogue/Mappilogue/Sources/Scene/Login/ViewController/SignUpCompletionViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 class SignUpCompletionViewController: BaseViewController {
     var onTapped: (() -> Void)?
     
-    private let completionRoundView = UIView()
+    private let stackView = UIStackView()
     private let completionImage = UIImageView()
+    private let labelStackView = UIStackView()
     private let completionLabel = UILabel()
     private let goToHomeLabel = UILabel()
     
@@ -24,14 +25,20 @@ class SignUpCompletionViewController: BaseViewController {
     override func setupProperty() {
         super.setupProperty()
     
-        completionRoundView.layer.cornerRadius = 35 / 2
-        completionRoundView.layer.borderWidth = 2
-        completionRoundView.layer.borderColor = UIColor.color707070.cgColor
-        completionRoundView.backgroundColor = .clear
-    
-        completionImage.image = UIImage(named: "login_completion")
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.spacing = 40
         
-        completionLabel.text = "회원가입이 완료되었어요"
+        completionImage.image = UIImage(named: "login_completion")
+        completionImage.contentMode = .scaleAspectFit
+        
+        labelStackView.axis = .vertical
+        labelStackView.distribution = .equalSpacing
+        labelStackView.alignment = .center
+        labelStackView.spacing = 3
+        
+        completionLabel.text = "맵필로그에 오신 걸 환영해요!"
         completionLabel.textColor = .color1C1C1C
         completionLabel.font = .title01
         
@@ -43,35 +50,24 @@ class SignUpCompletionViewController: BaseViewController {
     override func setupHierarchy() {
         super.setupHierarchy()
 
-        view.addSubview(completionRoundView)
-        completionRoundView.addSubview(completionImage)
-        view.addSubview(completionLabel)
-        view.addSubview(goToHomeLabel)
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(completionImage)
+        stackView.addArrangedSubview(labelStackView)
+        labelStackView.addArrangedSubview(completionLabel)
+        labelStackView.addArrangedSubview(goToHomeLabel)
     }
     
     override func setupLayout() {
         super.setupLayout()
         
-        completionRoundView.snp.makeConstraints {
-            $0.centerY.equalToSuperview().offset(-35)
+        stackView.snp.makeConstraints {
+            $0.centerY.equalToSuperview().offset(-20)
             $0.centerX.equalToSuperview()
-            $0.width.height.equalTo(35)
         }
         
         completionImage.snp.makeConstraints {
-            $0.center.equalTo(completionRoundView)
-            $0.width.equalTo(17)
-            $0.height.equalTo(10)
-        }
-        
-        completionLabel.snp.makeConstraints {
-            $0.top.equalTo(completionRoundView.snp.bottom).offset(25)
-            $0.centerX.equalToSuperview()
-        }
-        
-        goToHomeLabel.snp.makeConstraints {
-            $0.top.equalTo(completionRoundView.snp.bottom).offset(55)
-            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(104)
         }
     }
     
