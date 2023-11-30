810bb00b0e547664b032ae00cbaafbea791c4852
hyemi <hyem0127@gmail.com>
2023-11-30 21:37:34 +0900

    chore: Custom Framework 추가

---
+++ Mappilogue/MappilgueKit/MappilgueKit/MappilgueKit.h
@@ -0,0 +1,18 @@
+//
+//  MappilgueKit.h
+//  MappilgueKit
+//
+//  Created by hyemi on 2023/11/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MappilgueKit.
+FOUNDATION_EXPORT double MappilgueKitVersionNumber;
+
+//! Project version string for MappilgueKit.
+FOUNDATION_EXPORT const unsigned char MappilgueKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MappilgueKit/PublicHeader.h>
+
+
