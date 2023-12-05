0ff551d9eddc6f793d09d415d3b41d64fe8cfed5
hyemi <hyem0127@gmail.com>
2023-12-05 12:31:49 +0900

    rename: 파일 이동

--- Mappilogue/Mappilogue/Sources/Global/Bundle.swift
+++ Mappilogue/Mappilogue/Sources/Global/Bundle.swift
@@ -8,6 +8,24 @@
 import Foundation
 
 extension Bundle {
+    var apiKey: String {
+        guard let file = Bundle.main.path(forResource: "SecureAPIKeys", ofType: "plist"),
+              let resource = NSDictionary(contentsOfFile: file),
+              let key = resource["API_KEY"] as? String else {
+            fatalError("Couldn't find file 'SecureAPIKeys.plist'.")
+        }
+        return key
+    }
+    
+    var kakaoApiKey: String {
+        guard let file = Bundle.main.path(forResource: "SecureAPIKeys", ofType: "plist"),
+              let resource = NSDictionary(contentsOfFile: file),
+              let key = resource["KAKAO_API_KEY"] as? String else {
+            fatalError("Couldn't find file 'SecureAPIKeys.plist'.")
+        }
+        return key
+    }
+    
     var kakaoRestKey: String {
         guard let file = Bundle.main.path(forResource: "SecureAPIKeys", ofType: "plist"),
               let resource = NSDictionary(contentsOfFile: file),
