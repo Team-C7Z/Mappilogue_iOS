8164a4d5cb50b265e3e06d1644706a1a1f7dd895
hyemi <hyem0127@gmail.com>
2023-12-07 01:15:06 +0900

    refactor: Environment 코드 수정

---
+++ Mappilogue/Mappilogue/Sources/Global/Environment.swift
@@ -0,0 +1,14 @@
+//
+//  Environment.swift
+//  Mappilogue
+//
+//  Created by hyemi on 2023/12/07.
+//
+
+import Foundation
+
+struct Environment {
+    static let baseURL = Bundle.main.apiKey
+    static let kakaoAPI = Bundle.main.kakaoApiKey
+    static let kakaoRestKey = Bundle.main.kakaoRestKey
+}
